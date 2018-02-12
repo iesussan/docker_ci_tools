7ba2cd30029284cc45d24862f79e180fa71465fc
Daimonos <sanchezajesus@gmail.com>
2018-02-11 23:33:49 -0500

    fix

--- jenkins.sh
+++ jenkins.sh
@@ -1,30 +1,4 @@
 #! /bin/bash -e
-
-if [[ ! -z $(which java) ]]  && [[ $(which java) == '/usr/bin/java' ]]
-  then
-readonly jdk_download_url2=$(
-    curl -s $JAVA_DOWNLOAD_URL1 | \
-    egrep -o "\/technetwork\/java/\javase\/downloads\/jdk${JAVA_JDK_VERSION}-downloads-.+?\.html" | \
-    head -1 | \
-    cut -d '"' -f 1
-)
-[[ -z "$jdk_download_url2" ]] && echo "Could not get jdk download url - $JAVA_DOWNLOAD_URL1" >> /dev/stderr
-
-readonly jdk_download_url3="${JAVA_BASE_URL}${jdk_download_url2}"
-readonly jdk_download_url4=$(
-    curl -s $jdk_download_url3 | \
-    egrep -o "http\:\/\/download.oracle\.com\/otn-pub\/java\/jdk\/[8-9](u[0-9]+|\+).*\/jdk-${JAVA_JDK_VERSION}.*(-|_)linux-(x64|x64_bin).${JAVA_PACKAGE_EXT}"
-)
-
-for dl_url in ${jdk_download_url4[@]}; do
-    wget --no-cookies \
-         --no-check-certificate \
-         --header "Cookie: oraclelicense=accept-securebackup-cookie" \
-         -N $dl_url
-done
-
-fi
-
 : "${JENKINS_HOME:="/var/jenkins_home"}"
 touch "${COPY_REFERENCE_FILE_LOG}" || { echo "Can not write to ${COPY_REFERENCE_FILE_LOG}. Wrong volume permissions?"; exit 1; }
 echo "--- Copying files at $(date)" >> "$COPY_REFERENCE_FILE_LOG"
